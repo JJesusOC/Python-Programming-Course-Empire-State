4570ce11d9cf54f3d42bd1a8fa2abe6832c343ba
JJesus <jjesuseoc01@gmail.com>
2026-01-19 23:45:09 -0500

    Add files via upload
    This is an initial assignment for our first module.

---
+++ Greeting and Calculation assignment.py
@@ -0,0 +1,14 @@
+# 1) Greeting for the first part
+print ("Hello, my name is JJesus and I am learning Python. :)")
+
+# 2) Sample calculation: one addition and one multiplication.
+x = 5
+y = 2
+a = 33
+b = 34
+
+result1 = x * y
+result2 = a + b 
+
+print (f"The product of {x} and {y} is {result1}.")
+print (f"The sum of {a} and {b} is {result2}.")
